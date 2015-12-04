1e2046c4389116cf030d7a537cf3a3391ff08fc2
me <me@place.com>
2015-12-04 15:48:19 -0600

    search all feature works

--- app/controllers/objects_controller.rb
+++ app/controllers/objects_controller.rb
@@ -1,6 +1,12 @@
 class ObjectsController < ApplicationController
+  def get_query
+
+  end
+
   def search
     query_string = params[:query_string]
-    @songs = Song.where query_string
+    @songs = Song.similar_to query_string
+    @albums = Album.similar_to query_string
+    @artists = Artist.similar_to query_string
   end
 end
